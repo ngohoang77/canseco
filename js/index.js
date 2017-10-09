094ce3509caecbf1448ffc9ced663e7288d23833
ngohoang77 <ngohoangno@gmail.com>
2017-10-09 08:32:32 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$('.slidecheck').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+$('.slidetop').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+});
